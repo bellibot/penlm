4acd8882869533221a083b0bc2ac7ad83ace6d6e
Edoardo Belli <72514759+bellibot@users.noreply.github.com>
2021-12-18 16:57:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from os import path
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'penlm',
   packages = ['penlm'],
-  version = '0.1', 
+  version = 'v1.0.2', 
   license='MIT',
   description = 'Penalized Linear Models for Classification and Regression',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   author = 'Edoardo Belli',
   author_email = 'iedobelli@gmail.com',
   url = 'https://github.com/bellibot/penlm',
-  download_url = 'https://github.com/bellibot/penlm/archive/refs/tags/v1.0.0.tar.gz',
   keywords = ['Classification', 'Regression', 'Linear', 'Penalty'],
+  python_requires='>=3.5',
   install_requires=[
           'numpy',
           'joblib',
@@ -18,13 +27,5 @@ setup(
       ],
   classifiers=[
     'Development Status :: 5 - Production/Stable',   # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
-    'Intended Audience :: Developers',               # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.8',
   ],
 )
